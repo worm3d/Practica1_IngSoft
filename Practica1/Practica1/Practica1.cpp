6353d4ee582284839e5388f33291b228fda1df09
Pablo Pla Gutiérrez <pablo.pla@live.u-tad.com>
2016-11-15 20:42:25 +0100

    Added functions

--- Practica1/Practica1/Practica1.cpp
+++ Practica1/Practica1/Practica1.cpp
@@ -2,53 +2,71 @@
 #define WORLDSIZE 80
 #define GAMEWAIT 39.998
 
+int characterPos = 40;
+int bulletPos = 40;
+bool bullet = false;	//Indicates if the bullet is moving
+bool bulletDir = false;	//Indicates the direction of the bullet, false is left, and true is right
+
+void readInput()
+{
+
+	if (_kbhit()) {
+		char c = _getch();
+
+		if (c == 'a' && characterPos > 0)				characterPos--;
+		else if (c == 'd' && characterPos < WORLDSIZE)	characterPos++;
+		else if (c == 'q' && !bullet) {
+			bulletPos--;
+			bulletDir = false;
+			bullet = true;
+		}
+		else if (c == 'e' && !bullet) {
+			bulletPos++;
+			bulletDir = true;
+			bullet = true;
+		}
+	}
+}
+
+//This function updates the state of the bullet and its position
+void updateBullet()
+{
+
+	if (!bullet) bulletPos = characterPos; //If a bullet is not moving, its position must be equal to the character position
+	else if (bullet) {
+		if (!bulletDir)	bulletPos--;
+		else bulletPos++;
+	}
+
+	if (bulletPos <= 0 || bulletPos >= WORLDSIZE) { //If the bullet is on the limits of the world, it dissapears and the position goes back to the player
+		bullet = false;
+		bulletPos = characterPos;
+	}
+}
+
+void printWorld()
+{
+	for (int i = 0; i < WORLDSIZE; i++)
+	{
+		if (i == characterPos)				printf("^.^");	//Prints the character
+		else if (i == bulletPos && bullet)	printf("-");	//Prints the bullet
+		else								printf("_");	//Prints the world's floor
+	}
+}
+
 int main()
 {
-	int characterPos = 40;
-	int bulletPos = 40;
-	bool bullet = false; //Indicates if the bullet is moving
-	bool bulletDir = false; //Indicates the direction of the bullet, false is left, and true is right
-
 	printf("\n\n\n\n\n\n\n"); //Move the cursor a bit down from initial position
 
 	while (true)
 	{
-		if (!bullet) bulletPos = characterPos; //If a bullet is not moving, its position must be equal to the character position
-		else if (bullet) {
-			if (!bulletDir)	bulletPos--;
-			else bulletPos++;
-		}
+		updateBullet();
 
-		if (bulletPos <= 0 || bulletPos >= WORLDSIZE) {
-			bullet = false;
-			bulletPos = characterPos;
-		}
-
-		for (int i = 0; i < WORLDSIZE; i++)
-		{
-			if (i == characterPos)				printf("^.^");	//Prints the character
-			else if (i == bulletPos && bullet)	printf("-");	//Prints the bullet
-			else								printf("_");	//Prints the world's floor
-		}
+		printWorld();
 
 		printf("\r"); //Returns the cursor to the beginning of the line
 
-		if (_kbhit()) {
-			char c = _getch();
-
-			if (c == 'a' && characterPos > 0)				characterPos--;
-			else if (c == 'd' && characterPos < WORLDSIZE)	characterPos++;
-			else if (c == 'q' && !bullet) {
-				bulletPos--;
-				bulletDir = false;
-				bullet = true;
-			}
-			else if (c == 'e' && !bullet) {
-				bulletPos++;
-				bulletDir = true;
-				bullet = true;
-			}
-		}
+		readInput();
 
 		Sleep(GAMEWAIT);
 	}
